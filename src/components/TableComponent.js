983426c04e5370b3703d87fbbc59bd363dc66a6b
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-09-24 23:38:22 +0800

    Added pages

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -10,15 +10,34 @@ import {
   Button,
   TextField,
 } from "@mui/material";
-import { Check, Close, Delete, Edit } from "@mui/icons-material";
+import {
+  ArrowBackIos,
+  ArrowForwardIos,
+  BackupTable,
+  Check,
+  Close,
+  Delete,
+  Edit,
+} from "@mui/icons-material";
 import axios from "axios";
 
 function TableComponent() {
   const [data, setData] = useState([]);
   const [editable, setEditable] = useState({});
   const initialEditedData = {}; // Initialize editedData as an empty object
-
   const [editedData, setEditedData] = useState(initialEditedData);
+  const [rowsPerPage] = useState(7); // Set the number of rows per page
+  const [currentPage, setCurrentPage] = useState(1); // Current page
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
 
   useEffect(() => {
     fetchData();
@@ -116,159 +135,194 @@ function TableComponent() {
   };
 
   return (
-    <div className="table-container">
-      <TableContainer>
-        <Table className="table">
-          <TableHead>
-            <TableRow className="table-row">
-              <TableCell className="column-cell id">ID</TableCell>
-              <TableCell className="column-cell name">NAME</TableCell>
-              <TableCell className="column-cell date">DATE</TableCell>
-              <TableCell className="column-cell due">DUE</TableCell>
-              <TableCell className="column-cell fee">FEE (₱)</TableCell>
-              <TableCell className="column-cell contact">CONTACT NO</TableCell>
-              <TableCell className="column-cell email">EMAIL</TableCell>
-              <TableCell className="column-cell status">STATUS</TableCell>
-              <TableCell className="column-cell action">ACTION</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((item) => (
-              <TableRow className="table-body-row" key={item.ID}>
-                <TableCell className="table-cell id">
-                  <input className="input" value={item.ID} disabled />
+    <div>
+      <div className="table-container">
+        <TableContainer>
+          <Table className="table">
+            <TableHead>
+              <TableRow className="table-row">
+                <TableCell className="column-cell id">ID</TableCell>
+                <TableCell className="column-cell name">NAME</TableCell>
+                <TableCell className="column-cell date">DATE</TableCell>
+                <TableCell className="column-cell due">DUE</TableCell>
+                <TableCell className="column-cell fee">FEE (₱)</TableCell>
+                <TableCell className="column-cell contact">
+                  CONTACT NO
                 </TableCell>
-                <TableCell className="table-cell name">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.NAME || item.NAME}
-                      onChange={(e) => handleInputChange(e, item, "NAME")}
-                    />
-                  ) : (
-                    <input className="input" value={item.NAME} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell date">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.DATE || item.DATE}
-                      onChange={(e) => handleInputChange(e, item, "DATE")}
-                    />
-                  ) : (
-                    <input className="input" value={item.DATE} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell due">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.DUE || item.DUE}
-                      onChange={(e) => handleInputChange(e, item, "DUE")}
-                    />
-                  ) : (
-                    <input className="input" value={item.DUE} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell fee">
-                  {editable[item.ID] ? (
-                    <>
+                <TableCell className="column-cell email">EMAIL</TableCell>
+                <TableCell className="column-cell status">STATUS</TableCell>
+                <TableCell className="column-cell action">ACTION</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.slice(startIndex, endIndex).map((item) => (
+                <TableRow className="table-body-row" key={item.ID}>
+                  <TableCell className="table-cell id">
+                    <input className="input" value={item.ID} disabled />
+                  </TableCell>
+                  <TableCell className="table-cell name">
+                    {editable[item.ID] ? (
                       <input
                         className="input"
-                        value={editedData[item.ID]?.FEE || item.FEE}
-                        onChange={(e) => handleInputChange(e, item, "FEE")}
+                        value={editedData[item.ID]?.NAME || item.NAME}
+                        onChange={(e) => handleInputChange(e, item, "NAME")}
                       />
-                    </>
-                  ) : (
-                    <input className="input" value={item.FEE} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell contact">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.CONTACT_NO || item.CONTACT_NO}
-                      onChange={(e) => handleInputChange(e, item, "CONTACT_NO")}
-                    />
-                  ) : (
-                    <input className="input" value={item.CONTACT_NO} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell email">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.EMAIL || item.EMAIL}
-                      onChange={(e) => handleInputChange(e, item, "EMAIL")}
-                    />
-                  ) : (
-                    <input className="input" value={item.EMAIL} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell status">
-                  {editable[item.ID] ? (
-                    <input
-                      className="input"
-                      value={editedData[item.ID]?.STATUS || item.STATUS}
-                      onChange={(e) => handleInputChange(e, item, "STATUS")}
-                    />
-                  ) : (
-                    <input className="input" value={item.STATUS} disabled />
-                  )}
-                </TableCell>
-                <TableCell className="table-cell-button">
-                  {editable[item.ID] ? (
-                    <>
-                      <Button
-                        variant="contained"
-                        style={{ backgroundColor: "green", marginRight: 10 }}
-                        onClick={() => {
-                          // First toggle the edit mode
-                          toggleEdit(item.ID);
-                          // Then submit the changes
-                          handleSubmit(item);
-                        }}
-                      >
-                        <Check />
-                      </Button>
-                      <Button
-                        variant="contained"
-                        style={{ backgroundColor: "", marginRight: 10 }}
-                        onClick={() => toggleEdit(item.ID)}
-                      >
-                        <Close />
-                      </Button>
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        variant="contained"
-                        style={{ backgroundColor: "#a389ec", marginRight: 10 }}
-                        onClick={() => toggleEdit(item.ID)}
-                      >
-                        <Edit />
-                      </Button>
-                      <Button
-                        variant="contained"
-                        style={{ backgroundColor: "red", marginRight: 10 }}
-                        onClick={() => {
-                          // First, handle the delete operation
-                          handleDelete(item);
-                          // Then toggle the edit mode
-                          toggleEdit(item.ID);
-                        }}
-                      >
-                        <Delete />
-                      </Button>
-                    </>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    ) : (
+                      <input className="input" value={item.NAME} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell date">
+                    {editable[item.ID] ? (
+                      <input
+                        className="input"
+                        value={editedData[item.ID]?.DATE || item.DATE}
+                        onChange={(e) => handleInputChange(e, item, "DATE")}
+                      />
+                    ) : (
+                      <input className="input" value={item.DATE} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell due">
+                    {editable[item.ID] ? (
+                      <input
+                        className="input"
+                        value={editedData[item.ID]?.DUE || item.DUE}
+                        onChange={(e) => handleInputChange(e, item, "DUE")}
+                      />
+                    ) : (
+                      <input className="input" value={item.DUE} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell fee">
+                    {editable[item.ID] ? (
+                      <>
+                        <input
+                          className="input"
+                          value={editedData[item.ID]?.FEE || item.FEE}
+                          onChange={(e) => handleInputChange(e, item, "FEE")}
+                        />
+                      </>
+                    ) : (
+                      <input className="input" value={item.FEE} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell contact">
+                    {editable[item.ID] ? (
+                      <input
+                        className="input"
+                        value={
+                          editedData[item.ID]?.CONTACT_NO || item.CONTACT_NO
+                        }
+                        onChange={(e) =>
+                          handleInputChange(e, item, "CONTACT_NO")
+                        }
+                      />
+                    ) : (
+                      <input
+                        className="input"
+                        value={item.CONTACT_NO}
+                        disabled
+                      />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell email">
+                    {editable[item.ID] ? (
+                      <input
+                        className="input"
+                        value={editedData[item.ID]?.EMAIL || item.EMAIL}
+                        onChange={(e) => handleInputChange(e, item, "EMAIL")}
+                      />
+                    ) : (
+                      <input className="input" value={item.EMAIL} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell status">
+                    {editable[item.ID] ? (
+                      <input
+                        className="input"
+                        value={editedData[item.ID]?.STATUS || item.STATUS}
+                        onChange={(e) => handleInputChange(e, item, "STATUS")}
+                      />
+                    ) : (
+                      <input className="input" value={item.STATUS} disabled />
+                    )}
+                  </TableCell>
+                  <TableCell className="table-cell-button">
+                    {editable[item.ID] ? (
+                      <>
+                        <Button
+                          variant="contained"
+                          style={{ backgroundColor: "green", marginRight: 10 }}
+                          onClick={() => {
+                            // First toggle the edit mode
+                            toggleEdit(item.ID);
+                            // Then submit the changes
+                            handleSubmit(item);
+                          }}
+                        >
+                          <Check />
+                        </Button>
+                        <Button
+                          variant="contained"
+                          style={{ backgroundColor: "", marginRight: 10 }}
+                          onClick={() => toggleEdit(item.ID)}
+                        >
+                          <Close />
+                        </Button>
+                      </>
+                    ) : (
+                      <>
+                        <Button
+                          variant="contained"
+                          style={{
+                            backgroundColor: "#a389ec",
+                            marginRight: 10,
+                          }}
+                          onClick={() => toggleEdit(item.ID)}
+                        >
+                          <Edit />
+                        </Button>
+                        <Button
+                          variant="contained"
+                          style={{ backgroundColor: "red", marginRight: 10 }}
+                          onClick={() => {
+                            // First, handle the delete operation
+                            handleDelete(item);
+                            // Then toggle the edit mode
+                            toggleEdit(item.ID);
+                          }}
+                        >
+                          <Delete />
+                        </Button>
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div className="pagination">
+        <div className="label-page">
+          <Button
+            style={{ backgroundColor: "transparent", border: 0 }}
+            disabled={currentPage === 1}
+            onClick={prevPage}
+          >
+            <ArrowBackIos></ArrowBackIos>
+          </Button>
+          <span style={{ margin: "0 10px" }}>{currentPage}</span>{" "}
+          {/* Display current page */}
+          <Button
+            style={{ backgroundColor: "transparent", border: 0 }}
+            disabled={endIndex >= data.length}
+            onClick={nextPage}
+          >
+            <ArrowForwardIos></ArrowForwardIos>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
